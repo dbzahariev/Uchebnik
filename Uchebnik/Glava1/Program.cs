65748682bc9a010036d4bfc5102b4eef1f4b7453
Dimitar Zahariev <dbzahariev@gmail.com>
2017-04-18 19:31:43 +0300

    Add new task

--- Uchebnik/Glava1/Program.cs
+++ Uchebnik/Glava1/Program.cs
@@ -10,12 +10,19 @@ namespace Glava1
     {
         static void Main(string[] args)
         {
-            Task6();
+            Task7();
         }
 
         static void Task6()
         {
             Console.WriteLine("Dimitar Zahariev");
+        }
+
+        static void Task7()
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
         }
     }
 }
